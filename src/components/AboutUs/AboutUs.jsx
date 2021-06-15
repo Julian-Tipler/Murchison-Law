c86bf3aafa64e315175d132efd50d17dedf19a18
Julian <tipler.julian@gmail.com>
2021-06-14 22:07:12 -0700

    daily

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function AboutUs() {
-    return (
-        <div>
-            AboutUs Page
-        </div>
-    )
+  const [useCategory, setCategory] = useState(
+    "Welcome to Murchison & Cumming, LLP"
+  );
+
+  const categoryClick = (e) => {
+    setCategory(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>Title</div>
+      <div className="sidebar">
+        <div onClick={categoryClick}>Welcome to Murchison & Cumming, LLP</div>
+        <div onClick={categoryClick}>Litigation Philosophy</div>
+        <div onClick={categoryClick}>Billing Philosophy</div>
+        <div onClick={categoryClick}>Professional Organizations</div>
+        <div onClick={categoryClick}>Representative Clients</div>
+        <div onClick={categoryClick}>Diversity, Equity, & Inclusion</div>
+        <div onClick={categoryClick}>Making A Difference</div>
+        <div onClick={categoryClick}>Administrative Contacts</div>
+      </div>
+    </div>
+  );
 }
--- src/components/PracticeIndustries/PracticeIndustries.jsx
+++ src/components/PracticeIndustries/PracticeIndustries.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
+import Category from "./Category"
 
 export default function PracticeIndustries() {
-    const [useCategory, setCategory] = useState('')
+  const [useCategory, setCategory] = useState("");
 
-    const categoryClick = e => {
-        setCategory(e.target.value)
-    }
-    console.log(useCategory)
-    return (
+  const categoryClick = (e) => {
+    setCategory(e.target.value); 
+  };
+
+  return (
+    <div>
+      <div>Practice & Industries Page</div>
       <div>
-        <div>Practice & Industries Page</div>
-        <div>
-          <div>Category Sidebar Header</div>
-          <div onClick={categoryClick}>Marijuana Law</div>
-          <div onClick={categoryClick}>Criminal Law</div>
-          <div onClick={categoryClick}>Traffic Law</div>
-        </div>
-        <div>
-          <Category searchTerm={useCategory}/>
-        </div>
+        <div>Category Sidebar Header</div>
+        <div onClick={categoryClick}>Marijuana Law</div>
+        <div onClick={categoryClick}>Criminal Law</div>
+        <div onClick={categoryClick}>Traffic Law</div>
       </div>
-    );
+      <div>
+        <Category searchTerm={useCategory} />
+      </div>
+    </div>
+  );
 }
